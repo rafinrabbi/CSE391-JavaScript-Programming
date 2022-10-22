e158465d28b6213b26245b752e87c56590aade7b
Robotics Club of BRAC University <112168274+RobuLab@users.noreply.github.com>
2022-10-23 04:26:25 +0600

    first

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ window.onload = function name(){
     })
 }
 
+function blue() {
+    document.getElementById('demo').style.backgroundColor = "#051650";
+    document.getElementById('demo').style.borderColor = "orange";
+    document.getElementById('demo').style.fontFamily = "monospace";
+    document.getElementById('demo').style.color = "white";
+}
+function red() {
+    document.getElementById('demo').style.backgroundColor = "#9b0f2e";
+    document.getElementById('demo').style.borderColor = "black";
+    document.getElementById('demo').style.fontFamily = "Helvetica";
+    document.getElementById('demo').style.color = "white";
+}
+function green() {
+    document.getElementById('demo').style.backgroundColor = "#006e1f";
+    document.getElementById('demo').style.borderColor = "black";
+    document.getElementById('demo').style.fontFamily = "monospace";
+    document.getElementById('demo').style.color = "whitesmoke";
+}
+function black() {
+    document.getElementById('demo').style.backgroundColor = "#000000";
+    document.getElementById('demo').style.borderColor = "red";
+    document.getElementById('demo').style.fontFamily = "sans-serif";
+    document.getElementById('demo').style.color = "white";
+}
--- script4.js
+++ script4.js
@@ -16,3 +16,28 @@ function reverse(){
     // console.log(text);
     document.getElementById('textarea').value = text.split('\n').reverse().join('\n');
 }
+
+function stripBlank(){
+    var text = document.getElementById('textarea').value;
+    document.getElementById('textarea').value = text.split(/\r?\n/) 
+    .filter(line => line.trim() !== "") 
+    .join("\n"); 
+}
+
+function addNumber() {
+    var text = document.getElementById('textarea').value;
+    var arr = text.split('\n');
+    arr = arr.map(i => Math.floor(Math.random()*10+1).toString() +" " + i);
+    document.getElementById('textarea').value = arr.toString().split(',').join('\n');
+}
+
+function shuffle() {
+    var text = document.getElementById('textarea').value;
+    var arr = text.split('\n');
+    arr = arr.sort(function (a, b) {
+        return Math.random()-0.5;
+    });
+        
+    console.log(arr)
+    document.getElementById('textarea').value = arr.toString().split(',').join('\n');
+}
